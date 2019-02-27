a357439e5885c39e0ee5026e616e6bf0907f14e3
RyanMcGlinnGDD <ryanmcglinngdd@gmail.com>
2019-02-27 12:08:35 -0500

    Initial commit
    Added starting files to repository

---
+++ content/actor/Freddie/expressions.js
@@ -0,0 +1,27 @@
+{
+    "name": "Freddie",
+    "expressions":[
+        {
+            "type": "freddie_neutral"
+        },
+        {
+            "type": "freddie_happy"
+        },
+        {
+            "type": "freddie_angry"
+        },
+        {
+            "type": "freddie_nope"
+        },
+        {
+            "type": "freddie_question"
+        },
+        {
+            "type": "freddie_questionreverse"
+        },
+        {
+            "type": "freddie_exclaim"
+        }
+    ]
+}
+
